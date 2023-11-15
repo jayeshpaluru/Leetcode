36856b42a430a23ea0d85eb4303c0e5fe4e9112c
Jayesh Paluru <jayeshpaluru@gmail.com>
2023-11-14 23:16:06 -0600

    Did Maximum Element After Decreasing and Rearranging

---
+++ Problems/MaximumElementAfterDecreasingandRearranging.go
@@ -0,0 +1,23 @@
+package main
+
+// Given an array of integers arr
+// Perform some operations (possibly none) on arr so that it satisfies these conditions:
+// 1. The value of the first element in arr must be 1.
+// 2. The absolute difference between any 2 adjacent elements must be less than or equal to 1.
+// There are 2 types of operations that you can perform any number of times:
+// 1. Decrease the value of any element of arr to a smaller positive integer.
+// 2. Rearrange the elements of arr to be in any order.
+// Return the maximum possible value of an element in arr after performing the operations to satisfy the conditions.
+import "sort"
+
+// This solution works by sorting the array and then iterating through it.
+func maximumElementAfterDecrementingAndRearranging(arr []int) int {
+	sort.Ints(arr)
+	arr[0] = 1
+	for i := 1; i < len(arr); i++ {
+		if arr[i]-arr[i-1] > 1 {
+			arr[i] = arr[i-1] + 1
+		}
+	}
+	return arr[len(arr)-1]
+}
--- README.md
+++ README.md
@@ -16,3 +16,5 @@
 - H-Index (11/12/2023 #274)
 - Insert Delete GetRandom O(1) (11/13/2023 #380)
 - Product of Array Except Self (11/13/2023 #238)
+- Unique Length-3 Palindromic Subsequences (11/13/2023 #1930)
+- Maximum Element After Decreasing and Rearranging (11/14/2023 #1846)
